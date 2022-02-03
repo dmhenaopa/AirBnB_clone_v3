dca5fda6671cff215dc87c28ba4db45ec235c246
Andres Castañeda <andrescastanedaxy@gmail.com>
2022-02-03 12:13:01 -0500

    [ADD] - route stats added

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,6 +2,14 @@
 """Blueprint and routes"""
 from api.v1.views import app_views
 from flask import jsonify
+from api.v1.app import storage
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+from models.place import Place
+from models.user import User
+import json
 
 
 @app_views.route('/status', methods=['GET'])
@@ -10,3 +18,16 @@ def return_status():
     return jsonify({
         "status": "OK"
     })
+
+
+@app_views.route('/stats', methods=['GET'])
+def stats():
+    """Return number of objects by type"""
+    classes = {"amenities": Amenity, "cities": City,
+               "places": Place, "reviews": Review, "states": State,
+               "users": User}
+    new_dict = {}
+    for clss in classes:
+        count_result = storage.count(classes[clss])
+        new_dict[clss] = count_result
+    return json.dumps(new_dict)
