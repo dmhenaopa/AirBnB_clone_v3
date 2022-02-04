2143299bb4d257f15c7f48355066a074e1ee1ba3
Andres Castañeda <andrescastanedaxy@gmail.com>
2022-02-03 20:23:02 -0500

    [FIX] - fix conditional no JSON and use of **dict()

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -43,35 +43,34 @@ def delete_state(states_id):
 @app_views.route('/states', methods=['POST'], strict_slashes=False)
 def create_state():
     """Create a new object State"""
-    request_data = request.get_json()
-
-    if 'name' in request_data:
-        name_state = request_data['name']
-    else:
-        return error_handler_400("Missing name")
-    new_state = State(name_state)
-    storage.new(new_state)
     try:
-        new_state.name = name_state
-        new_json = json.dumps(new_state.to_dict())
-        storage.save()
+        request_data = request.get_json()
     except Exception:
         return error_handler_400("Not a JSON")
+    if 'name' not in request_data:
+        return error_handler_400("Missing name")
+    info_state = dict(request_data)
+    new_state = State(**info_state)
+    storage.new(new_state)
+    new_json = json.dumps(new_state.to_dict())
+    storage.save()
     return new_json, 201
 
 
 @app_views.route('/states/<states_id>', methods=['PUT'], strict_slashes=False)
 def update_state(states_id):
     """Update information of an object State by id"""
-    request_data = request.get_json()
     object = storage.get(State, states_id)
     if object is None:
         return error_handler(object)
-    for key, value in dict(request_data).items():
-        setattr(object, key, value)
     try:
-        new_json = json.dumps(object.to_dict())
-        storage.save()
-        return new_json, 200
+        request_data = request.get_json()
     except Exception:
         return error_handler_400("Not a JSON")
+    ignore = ["id", "created_at", "updated_at"]
+    for key, value in dict(request_data).items():
+        if key not in ignore:
+            setattr(object, key, value)
+    new_json = json.dumps(object.to_dict())
+    storage.save()
+    return new_json, 200
