81ace92456080db27263996520178f328cbaa7aa
Andres Castañeda <andrescastanedaxy@gmail.com>
2022-02-07 12:21:02 -0500

    [UPD] - logic previusly

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 """Blueprint and routes"""
 from flask import request, abort, jsonify
-# from api.v1.app import *
-from api.v1.views import app_views
+from api.v1.app import *
+from api.v1.views.index import *
 from models.state import State
-from models import storage
 import json
 
 
